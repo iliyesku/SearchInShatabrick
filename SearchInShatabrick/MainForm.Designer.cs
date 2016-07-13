5a6935b4162f75dcb50f3fbfd1ec0968a8c2c3eb
iliyesku <iliyesku@gmail.com>
2016-07-13 22:54:06 +0300

    Add files via upload

---
+++ SearchInShatabrick/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: iliyesku
+ * Date: 29.09.2013
+ * Time: 12:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace SearchInShatabrick
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "Double click to exit";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1MouseDoubleClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(0, 0);
+			this.ControlBox = false;
+			this.Enabled = false;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.Opacity = 0D;
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+
+		}
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+	}
+}
---
+++ SearchInShatabrick/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace SearchInShatabrick
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			FileSystemWatcher watcher = new FileSystemWatcher();
+			watcher.Path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+			watcher.IncludeSubdirectories = true;
+			
+			watcher.NotifyFilter = NotifyFilters.Size;
+
+			watcher.Filter = "IgnorePref*.ini";
+
+			watcher.Changed += new FileSystemEventHandler(OnChanged);
+			watcher.Created += new FileSystemEventHandler(OnChanged);
+
+			watcher.EnableRaisingEvents = true;
+		}
+		
+		private static void OnChanged(object source, FileSystemEventArgs e)
+		{
+			try
+			{
+				string s = File.ReadAllText(e.FullPath);
+				if (s != string.Empty)
+				{
+					Process.Start("http://www.shatabrick.com/cco/kw/index.php?g=kw&a=sp&name=" + s.Remove(0, s.IndexOf('=')+2));
+				}
+			}
+			catch
+			{
+				MessageBox.Show("Unable to process file: " +  e.FullPath/* + " " + e.ChangeType + " " + "Content: " + s*/);
+			}
+		}
+		
+		void NotifyIcon1MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
